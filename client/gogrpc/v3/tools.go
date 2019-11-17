af7cda1e59deaa5c358f2062359a0334c3ca9496
mactaggart <mactaggart@users.noreply.github.com>
2019-11-17 12:35:02 -0800

    reorder imports

--- client/gogrpc/v3/tools.go
+++ client/gogrpc/v3/tools.go
@@ -4,6 +4,6 @@ package tools
 
 import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
-	_ "github.com/square/goprotowrap/cmd/protowrap"
 	_ "github.com/kazegusuri/grpcurl"
+	_ "github.com/square/goprotowrap/cmd/protowrap"
 )
